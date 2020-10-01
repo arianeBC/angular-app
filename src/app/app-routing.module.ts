d2ecb24a8900d65a92d24140bdaff1106e48fe66
arianeBC <arianebrosseaucote@gmail.com>
2020-10-01 12:57:23 +1100

    Adding Editing Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
@@ -8,8 +9,10 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recettes', pathMatch: 'full' },
   { path: 'recettes', component: RecipesComponent, children: [
-    { path: '', component: RecipeStartComponent},
-    { path: ':id', component: RecipeDetailComponent}
+    { path: '', component: RecipeStartComponent },
+    { path: 'ajouter', component: RecipeEditComponent },
+    { path: ':id', component: RecipeDetailComponent },
+    { path: ':id/modifier', component: RecipeEditComponent },
   ] },
   { path: 'liste-de-courses', component: ShoppingListComponent },
 ];
