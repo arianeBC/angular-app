b8854982185842c6bc8a378bb08102a1f0213c17
arianeBC <arianebrosseaucote@gmail.com>
2020-10-13 10:27:46 +1100

    Allowing the User to Clear the form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -45,7 +45,10 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     form.reset();
   }
 
-
+  onClear() {
+    this.slForm.reset();
+    this.editMode = false;
+  }
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
