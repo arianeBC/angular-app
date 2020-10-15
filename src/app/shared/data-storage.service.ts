c0c4fdd2b21d70dc7b2a23fc4b330ddffe0db6e8
arianeBC <arianebrosseaucote@gmail.com>
2020-10-16 10:55:30 +1100

    Transforming Response Data

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
 
@@ -21,6 +22,16 @@ export class DataStorageService {
   fetchRecipes() {
     this.http
       .get<Recipe[]>('https://ng-recipe-book-9bbd7.firebaseio.com/recipes.json')
+      .pipe(
+        map(recipes => {
+          return recipes.map(recipe => {
+            return {
+              ...recipe, 
+              ingredients: recipe.ingredients ? recipe.ingredients : []
+            };
+          });
+        })
+      )
       .subscribe(recipes => {
         this.recipeService.setRecipes(recipes);
     });
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -7,10 +7,12 @@ export class ShoppingListService {
   ingredientsChanged = new Subject<Ingredient[]>();
   startedEditing = new Subject<number>();
 
-  private ingredients: Ingredient[] = [
-    new Ingredient('Carottes', 1),
-    new Ingredient('Tomates', 2),
-  ];
+  // private ingredients: Ingredient[] = [
+  //   new Ingredient('Carottes', 1),
+  //   new Ingredient('Tomates', 2),
+  // ];
+
+  private ingredients: Ingredient[] = [];
 
   getIngredients() {
     return this.ingredients.slice();
