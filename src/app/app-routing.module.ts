c21e42e698ed7fbf640c389a3945699b9c6e6ca3
arianeBC <arianebrosseaucote@gmail.com>
2020-10-16 11:27:07 +1100

    Resolving Data Before Loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,32 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { RecipeResolverRecipes } from './recipes/recipe-resolver.service';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
+
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recettes', pathMatch: 'full' },
-  { path: 'recettes', component: RecipesComponent, children: [
-    { path: '', component: RecipeStartComponent },
-    { path: 'ajouter', component: RecipeEditComponent },
-    { path: ':id', component: RecipeDetailComponent },
-    { path: ':id/modifier', component: RecipeEditComponent },
-  ] },
+  { 
+    path: 'recettes', 
+    component: RecipesComponent, 
+    children: [
+      { path: '', component: RecipeStartComponent },
+      { path: 'ajouter', component: RecipeEditComponent },
+      { 
+        path: ':id', 
+        component: RecipeDetailComponent, 
+        resolve: [RecipeResolverRecipes] 
+      },
+      { 
+        path: ':id/modifier', 
+        component: RecipeEditComponent,
+        resolve: [RecipeResolverRecipes] 
+      },
+    ] 
+  },
   { path: 'liste-de-courses', component: ShoppingListComponent },
 ];
 
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
 
@@ -20,7 +20,7 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http
+    return this.http
       .get<Recipe[]>('https://ng-recipe-book-9bbd7.firebaseio.com/recipes.json')
       .pipe(
         map(recipes => {
@@ -30,10 +30,10 @@ export class DataStorageService {
               ingredients: recipe.ingredients ? recipe.ingredients : []
             };
           });
+        }),
+        tap(recipes => {
+          this.recipeService.setRecipes(recipes);
         })
       )
-      .subscribe(recipes => {
-        this.recipeService.setRecipes(recipes);
-    });
   }
 }
