6015844260f7cf4c327d7ae76e6161407bfc46fc
arianeBC <arianebrosseaucote@gmail.com>
2020-10-19 11:55:32 +1100

    Login Error Handling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
@@ -18,38 +18,48 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   signup(email: string, password: string) {
-    return this.http.post<AuthResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBjF_me0299D9WO6zR-TWa8XqaByw7wOz4', 
-      {
-        email: email,
-        password: password,
-        returnSecureToken: true,
-      }
-    ).pipe(catchError(
-      errorRes => {
-        let errorMessage = 'Désolé, une erreur est survenue'
-        if(!errorRes.error || !errorRes.error.error) {
-          return throwError(errorMessage);
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBjF_me0299D9WO6zR-TWa8XqaByw7wOz4', 
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
         }
-        switch(errorRes.error.error.message) {
-          case 'EMAIL_EXISTS':
-            errorMessage = 'Cette adresse e-mail est déjà enregistrée';
-
-        }
-        return throwError(errorMessage);
-      }
-    ));
+      )
+      .pipe(catchError(this.handleError));
   }
 
   login(email: string, password: string) {
-    return this.http.post<AuthResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBjF_me0299D9WO6zR-TWa8XqaByw7wOz4'
-      ,
-      {
-        email: email,
-        password: password,
-        returnSecureToken: true,
-      }
-    );
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBjF_me0299D9WO6zR-TWa8XqaByw7wOz4'
+        ,
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = 'Désolé, une erreur est survenue'
+    if(!errorRes.error || !errorRes.error.error) {
+      return throwError(errorMessage);
+    }
+    switch(errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'Cette adresse e-mail est déjà enregistrée';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = "Votre nom d'utilisateur et votre mot de passe ne correspondent pas. Veuillez réessayer.";
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = "Votre nom d'utilisateur et votre mot de passe ne correspondent pas. Veuillez réessayer.";
+        break;
+    }
+    return throwError(errorMessage);
   }
 }
