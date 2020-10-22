f490bc2e12110b99d46b8d5088b8fa0e6ca4c56e
arianeBC <arianebrosseaucote@gmail.com>
2020-10-22 13:55:12 +1100

    Managing More State via NgRx

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -5,6 +5,7 @@ import { Ingredient } from '../shared/ingredient.model';
 import { Observable, Subscription } from 'rxjs';
 import { Store } from '@ngrx/store';
 import * as fromShoppingList from './store/shopping-list.reducer';
+import * as shoppingListActions from './store/shopping-list.action';
 
 @Component({
   selector: 'app-shopping-list',
@@ -32,7 +33,8 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   }
 
   onEditItem(index: number) {
-    this.slService.startedEditing.next(index);
+    // this.slService.startedEditing.next(index);
+    this.store.dispatch(new shoppingListActions.StartEdit(index));
   }
 
   ngOnDestroy() {
