a365b43caa08ee8afda08f2d7c180df0584cff6a
arianeBC <arianebrosseaucote@gmail.com>
2020-10-26 12:47:03 +1100

    Dispatching Auth Actions

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -5,6 +5,9 @@ import { BehaviorSubject, throwError } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
 import { User } from './user.model';
 import { environment } from '../../environments/environment';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../store/app.reducer';
+import * as AuthActions from './store/auth.actions';
 
 export interface AuthResponseData {
   idToken: string,
@@ -22,7 +25,8 @@ export class AuthService {
 
   constructor(
     private http: HttpClient,
-    private router: Router
+    private router: Router,
+    private store: Store<fromApp.AppState>
   ) {}
 
   signup(email: string, password: string) {
@@ -90,7 +94,15 @@ export class AuthService {
     );
 
     if(loadedUser.token) {
-      this.user.next(loadedUser);
+      // this.user.next(loadedUser);
+      this.store.dispatch(
+        new AuthActions.Login({
+          email: loadedUser.email, 
+          userID: loadedUser.id, 
+          token: loadedUser.token, 
+          expirationDate: new Date(userData._tokenExpirationDate)
+        })
+      );
       const expirationDuration = 
         new Date(userData._tokenExpirationDate).getTime() - 
         new Date().getTime();
@@ -99,7 +111,8 @@ export class AuthService {
   }
 
   logout() {
-    this.user.next(null);
+    // this.user.next(null);
+    this.store.dispatch(new AuthActions.Logout());
     this.router.navigate(['/connexion']);
     localStorage.removeItem('userData');
     if(this.tokenExpirationTimer) {
@@ -123,7 +136,15 @@ export class AuthService {
   ) {
     const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
     const user = new User(email, userId, token, expirationDate);
-    this.user.next(user);
+    // this.user.next(user);
+    this.store.dispatch(
+      new AuthActions.Login({
+        email: email,
+        userID: userId,
+        token: token,
+        expirationDate: expirationDate
+      })
+    )
     this.autoLogout(expiresIn * 1000);
     localStorage.setItem('userData', JSON.stringify(user));
   }
