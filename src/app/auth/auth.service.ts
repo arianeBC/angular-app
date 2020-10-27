5e1114d0ed34a88adccbe12bbaac9f1081bf19dd
arianeBC <arianebrosseaucote@gmail.com>
2020-10-27 14:04:12 +1100

    Adding Auto-Logout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,10 +1,4 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import { BehaviorSubject, throwError } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
-import { User } from './user.model';
-import { environment } from '../../environments/environment';
 import { Store } from '@ngrx/store';
 import * as fromApp from '../store/app.reducer';
 import * as AuthActions from './store/auth.actions';
@@ -20,104 +14,20 @@ export interface AuthResponseData {
 
 @Injectable({providedIn:'root'})
 export class AuthService {
-  // user = new BehaviorSubject<User>(null);
   private tokenExpirationTimer: any;
 
-  constructor(
-    private http: HttpClient,
-    private router: Router,
-    private store: Store<fromApp.AppState>
-  ) {}
+  constructor(private store: Store<fromApp.AppState>) { }
 
-  autoLogin() {
-    const userData: {
-      email: string;
-      id: string;
-      _token: string;
-      _tokenExpirationDate: string;
-    } = JSON.parse(localStorage.getItem('userData'));
-    if(!userData) {
-      return;
-    }
-
-    const loadedUser = new User(
-      userData.email, 
-      userData.id, 
-      userData._token, 
-      new Date(userData._tokenExpirationDate)
-    );
-
-    if(loadedUser.token) {
-      // this.user.next(loadedUser);
-      this.store.dispatch(
-        new AuthActions.AuthenticateSuccess({
-          email: loadedUser.email, 
-          userID: loadedUser.id, 
-          token: loadedUser.token, 
-          expirationDate: new Date(userData._tokenExpirationDate)
-        })
-      );
-      const expirationDuration = 
-        new Date(userData._tokenExpirationDate).getTime() - 
-        new Date().getTime();
-      this.autoLogout(expirationDuration);
-    }
-  }
-
-  logout() {
-    // this.user.next(null);
-    this.store.dispatch(new AuthActions.Logout());
-    localStorage.removeItem('userData');
-    if(this.tokenExpirationTimer) {
-      clearTimeout(this.tokenExpirationTimer);
-    }
-    this.tokenExpirationTimer = null;
-  }
-
-  autoLogout(expirationDuration: number) {
-    // console.log(expirationDuration);
+  setLogoutTimer(expirationDuration: number) {
     this.tokenExpirationTimer = setTimeout(() => {
-      this.logout();
+      this.store.dispatch(new AuthActions.Logout());
     }, expirationDuration);
   }
 
-  private handleAuthentication(
-    email: string, 
-    userId: string, 
-    token: string, 
-    expiresIn: number
-  ) {
-    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
-    const user = new User(email, userId, token, expirationDate);
-    // this.user.next(user);
-    this.store.dispatch(
-      new AuthActions.AuthenticateSuccess({
-        email: email,
-        userID: userId,
-        token: token,
-        expirationDate: expirationDate
-      })
-    )
-    this.autoLogout(expiresIn * 1000);
-    localStorage.setItem('userData', JSON.stringify(user));
-  }
-
-  private handleError(errorRes: HttpErrorResponse) {
-    let errorMessage = 'Désolé, une erreur est survenue'
-    if(!errorRes.error || !errorRes.error.error) {
-      return throwError(errorMessage);
+  clearLogoutTimer() {
+    if(this.tokenExpirationTimer) {
+      clearTimeout(this.tokenExpirationTimer);
+      this.tokenExpirationTimer = null;
     }
-    switch(errorRes.error.error.message) {
-      case 'EMAIL_EXISTS':
-        errorMessage = 'Cette adresse e-mail est déjà enregistrée';
-        break;
-      case 'EMAIL_NOT_FOUND':
-        errorMessage = "Votre nom d'utilisateur et votre mot de passe ne correspondent pas. Veuillez réessayer.";
-        break;
-      case 'INVALID_PASSWORD':
-        errorMessage = "Votre nom d'utilisateur et votre mot de passe ne correspondent pas. Veuillez réessayer.";
-        break;
-    }
-    return throwError(errorMessage);
   }
 }
--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -7,6 +7,7 @@ import { of, throwError } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from '../user.model';
+import { AuthService } from '../auth.service';
 
 export interface AuthResponseData {
   idToken: string,
@@ -69,6 +70,9 @@ export class AuthEffects {
           }
         )
         .pipe(
+          tap(resData => {
+            this.authService.setLogoutTimer(+resData.expiresIn * 1000);
+          }),
           map(resData => {
             return handleAuthentication(
               +resData.expiresIn, 
@@ -98,6 +102,9 @@ export class AuthEffects {
           }
         )
         .pipe(
+          tap(resData => {
+            this.authService.setLogoutTimer(+resData.expiresIn * 1000);
+          }),
           map(resData => {
             return handleAuthentication(
               +resData.expiresIn, 
@@ -115,7 +122,7 @@ export class AuthEffects {
 
   @Effect({dispatch: false})
   authRedirect = this.actions$.pipe(
-    ofType(AuthActions.AUTHENTICATE_SUCCESS, AuthActions.LOGOUT), 
+    ofType(AuthActions.AUTHENTICATE_SUCCESS), 
     tap(() => {
       this.router.navigate(['/']);
     })
@@ -143,7 +150,10 @@ export class AuthEffects {
       );
   
       if(loadedUser.token) {
-        // this.user.next(loadedUser);
+        const expirationDuration = 
+          new Date(userData._tokenExpirationDate).getTime() - 
+          new Date().getTime();
+        this.authService.setLogoutTimer(expirationDuration);
         return new AuthActions.AuthenticateSuccess({
           email: loadedUser.email, 
           userID: loadedUser.id, 
@@ -163,13 +173,16 @@ export class AuthEffects {
   authLogout = this.actions$.pipe(
     ofType(AuthActions.LOGOUT),
     tap(() => {
+      this.authService.clearLogoutTimer();
       localStorage.removeItem('userData');
+      this.router.navigate(['/connexion']);
     })
   )
 
   constructor(
     private actions$: Actions, 
     private http: HttpClient,
-    private router: Router
+    private router: Router,
+    private authService: AuthService
   ) { }
 }
