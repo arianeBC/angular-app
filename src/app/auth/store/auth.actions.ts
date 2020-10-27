ae1ea6a8f1ea39501c7e2f0c92b6376c2aa8ac7c
arianeBC <arianebrosseaucote@gmail.com>
2020-10-27 13:14:59 +1100

    Further Auth Effects

--- src/app/auth/store/auth.actions.ts
+++ src/app/auth/store/auth.actions.ts
@@ -4,6 +4,7 @@ export const LOGIN_START = '[Auth] Login Start';
 export const AUTHENTICATE_SUCCESS = '[Auth] Authenticate Success';
 export const AUTHENTICATE_FAIL = '[Auth] Authenticate Fail';
 export const SIGNUP_START = '[Auth] Signup Start';
+export const CLEAR_ERROR = '[Auth] Clear Error';
 export const LOGOUT = '[Auth] Logout';
 
 export class LoginStart implements Action {
@@ -37,6 +38,10 @@ export class SignupStart implements Action {
   constructor(public payload: {email: string, password: string}) { }
 }
 
+export class ClearError implements Action {
+  readonly type = CLEAR_ERROR;
+}
+
 export class Logout implements Action {
   readonly type = LOGOUT;
 }
@@ -46,4 +51,5 @@ export type AuthActions =
   | AuthenticateSuccess
   | AuthenticateFail
   | SignupStart
-  | Logout ;
+  | Logout 
+  | ClearError;
